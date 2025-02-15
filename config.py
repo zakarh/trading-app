56ff99a9b9ae71a4ce5c92b0c55c4b2dd558068a
zakarh <zakar.handricken@gmail.com>
2025-02-14 19:46:14 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 import os
 
 # API keys and configuration
-ALPACA_API_KEY = os.getenv('ALPACA_API_KEY', 'AKNZ2LJFKEG5WHYOC3YZ')
-ALPACA_API_SECRET = os.getenv('ALPACA_API_SECRET', 'bc4fpy2fXek1gFgzxEMv98e6Zj5Zj7u12Gtot3zA')
+ALPACA_API_KEY = os.getenv('ALPACA_API_KEY', '')
+ALPACA_API_SECRET = os.getenv('ALPACA_API_SECRET', '')
 ALPACA_BASE_URL = os.getenv('ALPACA_BASE_URL', 'https://paper-api.alpaca.markets')
 # ALPACA_BASE_URL = os.getenv('ALPACA_BASE_URL', 'https://api.alpaca.markets')
 
